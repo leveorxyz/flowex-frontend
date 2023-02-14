aded3c82c10dcd17fccd7075affa3e665d6d636a
Mahmudul Alam <expelmahmud@gmail.com>
2023-02-14 21:02:41 +0600

    Added footer

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,71 +1,51 @@
-import {
-  SimpleGrid,
-  Box,
-  Text,
-  VStack,
-  Heading,
-  Container,
-  Flex,
-  HStack,
-  Icon,
-} from '@chakra-ui/react';
-import { IoLogoInstagram, IoLogoFacebook, IoLogoTwitter } from 'react-icons/io5';
+import { Box, Text, Container, Flex, HStack, Icon, Divider } from '@chakra-ui/react';
+import { IoLogoInstagram, IoLogoFacebook, IoLogoTwitter, IoLogoYoutube } from 'react-icons/io5';
 import Logo from '../Logo/Logo';
+import MenuLink from '../MenuLink/MenuLink';
+import menuLinks from 'data/menuLinks.json';
 
 const Footer = () => {
   return (
     <Box as="footer">
-      <Container maxW="container.xl">
-        <SimpleGrid
-          columns={[1, 4, 5]}
-          spacing={10}
-          py={5}
-          mt={10}
-          borderBottom="1px solid"
-          borderBottomColor="gray.100"
-          color="white.100"
+      <Container
+        maxW="container.xl"
+        bg="gradient.footer"
+        borderTopRadius="40px"
+        py="10"
+        px={{ base: 5, md: 20 }}
+      >
+        <Flex justifyContent="space-between" gap={5} flexDir={{ base: 'column', md: 'row' }}>
+          <HStack gap={{ base: 5, md: 10 }} flexDir={{ base: 'column', md: 'row' }}>
+            {menuLinks.map((item) => (
+              <MenuLink key={item.name} text={item.name} link={item.link} />
+            ))}
+          </HStack>
+          <HStack gap="5" justifyContent={{ base: 'center', md: 'initial' }}>
+            <Icon as={IoLogoFacebook} fontSize="2xl" />
+            <Icon as={IoLogoTwitter} fontSize="2xl" />
+            <Icon as={IoLogoInstagram} fontSize="2xl" />
+            <Icon as={IoLogoYoutube} fontSize="2xl" />
+          </HStack>
+        </Flex>
+        <Divider borderColor="brand.gray" my={5} />
+        <Flex
+          justifyContent="space-between"
+          alignItems="center"
+          flexDir={{ base: 'column-reverse', md: 'row' }}
+          gap="5"
         >
-          <Box gridColumn={['1 span', '2 span']}>
-            <Logo fontSize="30px" />
-            <Text mt={5}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores deleniti, minus
-              quaerat voluptas quo tempore facilis commodi, officia accusantium odio optio velit,
-              architecto totam ad nobis tempora! Impedit, sed eos.
+          <Text fontSize="sm" fontWeight="medium">
+            &copy; {new Date().getFullYear()} Flowex. All rights reserved.
+          </Text>
+          <Logo />
+          <HStack gap="5">
+            <Text as="a" href="#!" fontSize="sm" fontWeight="medium">
+              Terms of Service
             </Text>
-          </Box>
-          <Box>
-            <VStack align="flex-start" gap="20px">
-              <Heading size="md">About</Heading>
-              <a href="#!">Product</a>
-              <a href="#!">Terms & Condition</a>
-              <a href="#!">FAQ</a>
-            </VStack>
-          </Box>
-          <Box>
-            <VStack align="flex-start" gap="20px">
-              <Heading size="md">Company</Heading>
-              <a href="#!">Our Team</a>
-              <a href="#!">Partner With Us</a>
-              <a href="#!">Privacy & Policy</a>
-            </VStack>
-          </Box>
-          <Box>
-            <VStack align="flex-start" gap="20px">
-              <Heading size="md">Contact</Heading>
-              <a href="#!">+1 339-707-5370</a>
-              <a href="mailto:contact@leveor.xyz">contact@leveor.xyz</a>
-            </VStack>
-          </Box>
-        </SimpleGrid>
-        <Flex justifyContent="space-between" py="5">
-          <Box>
-            <HStack gap="5">
-              <Icon as={IoLogoInstagram} />
-              <Icon as={IoLogoFacebook} />
-              <Icon as={IoLogoTwitter} />
-            </HStack>
-          </Box>
-          <Text fontSize="sm">Copyright {new Date().getFullYear()} IPPS</Text>
+            <Text as="a" href="#!" fontSize="sm" fontWeight="medium">
+              Privacy Policy
+            </Text>
+          </HStack>
         </Flex>
       </Container>
     </Box>
